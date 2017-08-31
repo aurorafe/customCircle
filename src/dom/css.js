2db29c9130978126af7942bd89f9f0cf9702bce3
zhangyp <zhangyp@hdsxtech.com>
2017-08-31 13:08:26 +0800

    添加编辑器 实现圆可拖拽

---
+++ src/dom/css.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @type {string}
+ */
+export const CLASS_CUSTOM_CIRCLE = 'custom-circle'
+/**
+ *
+ * @type {string}
+ */
+export const CLASS_CUSTOM_CIRCLE_BUTTON = 'custom-circle-button'
+/**
+ *
+ * @type {string}
+ */
+export const CLASS_CUSTOM_CIRCLE_HANDLELABEL = 'custom-circle-handleLabel'
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+/**
+ * Created by 张永平 on 2017/8/31.
+ * @desc 周边搜索 自定义圆
+ */
+import './scss/customCircle.scss'
+import * as htmlUtils from 'nature-dom-util/src/utils/domUtils'
+import * as utils from 'nature-dom-util/src/utils/utils'
+import * as css from './dom/css'
 ol.geom.CustomCircle = function (params) {
   /**
    * 当前配置
@@ -53,14 +61,30 @@ ol.geom.CustomCircle = function (params) {
    */
   this.sphare = new ol.Sphere(6378137)
   /**
+   * 鼠标是否按下
+   * @type {boolean}
+   */
+  this.isMouseDown = false
+  /**
+   * 是否正在移动
+   * @type {boolean}
+   */
+  this.isMoving = false
+  /**
    * 半径
    */
   this.radius = this.options['radius']
 
+  /**
+   * 半径显示DOM
+   * @type {null}
+   */
+  this.handleLabel = null
+
   this.initCircle(this.options['center'])
 }
 
-ol.inherits(ol.geom.Geometry, ol.geom.CustomCircle)
+ol.inherits(ol.geom.CustomCircle, ol.geom.Geometry)
 
 /**
  * 创建一个圆 创建一个要素
@@ -86,7 +110,19 @@ ol.geom.CustomCircle.prototype.initCircle = function (center) {
     source: new ol.source.Vector()
   })
   layer.getSource().addFeature(this.circleFeature)
+  /**
+   * 是否添加中心点
+   */
+  if (this.options['showCenterFeature']) {
+    this.addCenterPoint(layer)
+  }
   this.map.addLayer(layer)
+
+  /**
+   * 添加编辑操作
+   */
+  this.addEditor()
+  this.dispachChange()
 }
 
 /**
@@ -144,4 +180,137 @@ ol.geom.CustomCircle.prototype.zoomToExtent = function (extent) {
       size: size
     })
   }
+}
+
+/**
+ * 添加中心点
+ * @param layer
+ */
+ol.geom.CustomCircle.prototype.addCenterPoint = function (layer) {
+  this.centerPoint = new ol.Feature({
+    geometry: new ol.geom.Point(this.center)
+  })
+  let centerStyle = new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 5,
+      stroke: new ol.style.Stroke({
+        color: 'rgba(71, 129, 217, 1)',
+        width: 1
+      }),
+      fill: new ol.style.Fill({
+        color: 'rgba(255,255,255,0.5)'
+      })
+    })
+  })
+  this.centerPoint.setStyle(centerStyle)
+  layer.getSource().addFeature(this.centerPoint)
+}
+
+/**
+ * 添加编辑器
+ */
+ol.geom.CustomCircle.prototype.addEditor = function () {
+  let editor = htmlUtils.create('div', css.CLASS_CUSTOM_CIRCLE, document.body)
+  let button = htmlUtils.create('span', css.CLASS_CUSTOM_CIRCLE_BUTTON, editor, ('editor_' + utils.getuuid()))
+  this.handleLabel = htmlUtils.create('span', css.CLASS_CUSTOM_CIRCLE_HANDLELABEL, editor)
+  this.handleLabel.innerHTML = this.radius + 'm'
+  this.addEventHandle(button, this.handleLabel)
+  this.overlay = new ol.Overlay({
+    element: editor,
+    position: this.geom.getLastCoordinate(),
+    positioning: 'left-center',
+    offset: [-18, -10]
+  })
+  this.map.addOverlay(this.overlay)
+}
+
+/**
+ * 拖拽按钮事件处理
+ * @param button
+ */
+ol.geom.CustomCircle.prototype.addEventHandle = function (button) {
+  if (button && button instanceof Element) {
+    button.addEventListener('mousedown', event => {
+      if (event.preventDefault) {
+        event.preventDefault()
+      } else {
+        event.returnValue = false
+      }
+      this.isMouseDown = true
+      this.map.on('pointermove', this.onMouseMove, this)
+    })
+    button.addEventListener('mouseup', event => {
+      if (event.preventDefault) {
+        event.preventDefault()
+      } else {
+        event.returnValue = false
+      }
+      if (this.isMouseDown && this.isMoving) {
+        this.dispachChange()
+      }
+      this.isMouseDown = false
+      this.isMoving = false
+      this.map.un('pointermove', this.onMouseMove, this)
+    })
+    document.addEventListener('mouseup', event => {
+      if (event.preventDefault) {
+        event.preventDefault()
+      } else {
+        event.returnValue = false
+      }
+      if (this.isMouseDown && this.isMoving) {
+        this.dispachChange()
+      }
+      this.isMouseDown = false
+      this.isMoving = false
+      this.map.un('pointermove', this.onMouseMove, this)
+    })
+  }
+}
+
+/**
+ * 拖拽完成信息返回
+ */
+ol.geom.CustomCircle.prototype.dispachChange = function () {
+  if (this.options['onRadiusChangeEnd'] && typeof this.options['onRadiusChangeEnd'] === 'function') {
+    this.options['onRadiusChangeEnd'](this)
+  }
+}
+
+/**
+ * 鼠标移动
+ * @param event
+ */
+ol.geom.CustomCircle.prototype.onMouseMove = function (event) {
+  if (this.isMouseDown) {
+    this.mathRadius(event.coordinate)
+  }
+}
+
+/**
+ * 移动时 重新设置半径 重新构建圆 更新半径显示数值
+ * @param coords
+ */
+ol.geom.CustomCircle.prototype.mathRadius = function (coords) {
+  this.isMoving = true
+  if (this.center && coords) {
+    let c1 = ol.proj.transform(this.center, this._getProjectionCode(), 'EPSG:4326')
+    let c2 = ol.proj.transform(coords, this._getProjectionCode(), 'EPSG:4326')
+    let radius = this.sphare.haversineDistance(c1, c2)
+    if (radius > this.options['maxRadius']) {
+      this.radius = this.options['maxRadius'] - 1
+      this.isMouseDown = false
+      this.map.un('pointermove', this.onMouseMove, this)
+    } else if (radius < this.options['minRadius']) {
+      this.radius = this.options['minRadius'] - 1
+      this.isMouseDown = false
+      this.map.un('pointermove', this.onMouseMove, this)
+    } else {
+      this.radius = radius
+    }
+    this.handleLabel.innerHTML = Math.floor(this.radius) + 1 + 'm'
+    this.geom = this._getCircleGeom()
+    this.circleFeature.setGeometry(this.geom)
+  }
+  this.overlay.setPosition(this.geom.getLastCoordinate())
 }
